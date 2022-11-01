bd11d6014e27c8c77700ac0d135ce91df1aec7a3
Clasikpaige <102603571+Clasikpaige@users.noreply.github.com>
2022-11-01 15:36:40 +0100

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Paige Ado Henry';
+    : 'Paige Henry';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Software Developer';
