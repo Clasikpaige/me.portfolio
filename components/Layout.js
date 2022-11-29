536e718b1fb8fb6917244ddd6d656ca5ed2b7cbd
Paige <102603571+Clasikpaige@users.noreply.github.com>
2022-11-29 12:57:59 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 import { useEffect } from 'react';
 import styles from './Layout.module.css';
 
-export function BlueBackground({ variant, className }) {
+export function GradientBackground({ variant, className }) {
   const classes = classNames(
     {
       [styles.colorBackground]: variant === 'large',
