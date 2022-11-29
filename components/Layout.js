5bf7f02936ede38d8f0e011e494efa4421185b9d
Paige <102603571+Clasikpaige@users.noreply.github.com>
2022-11-29 13:00:05 +0100

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -5,8 +5,8 @@ import styles from './Layout.module.css';
 export function GradientBackground({ variant, className }) {
   const classes = classNames(
     {
-      [styles.colorBackground]: variant === 'large',
-      [styles.colorBackgroundBottom]: variant === 'small',
+      [styles.colorBackground]: blue === 'large',
+      [styles.colorBackgroundBottom]: blue === 'small',
     },
     className
   );
